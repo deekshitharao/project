db2dd121ebbd7f580437dc51bf563fab69d99bc7
deekshitharao <21eg107a43@anurag.edu.in>
2023-09-26 10:15:41 +0530

    fin

---
+++ app_api/model/db.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+let dbURI="mongodb://0.0.0.0:27017/loc8r";
+if (process.env.NODE_ENV === 'production') {
+  dbURI = process.env.MONGODB_URI;
+}
+mongoose.connect(dbURI);
+
+
+mongoose.connection.on('connected', () => {
+  console.log(`Mongoose connectedddddddddddddd ${dbURI}`);
+});
+mongoose.connection.on('error', err => {
+  console.log('Mongoose connection error:', err);
+});
+mongoose.connection.on('disconnected', () => {
+  console.log('Mongoose disconnected');
+});
+
+const gracefulShutdown = (msg, callback) => {
+  mongoose.connection.close( () => {
+    console.log(`Mongoose disconnected through ${msg}`);
+    callback();
+  });
+};
+
+
+require('./location');
+
